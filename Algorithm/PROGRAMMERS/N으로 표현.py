f839a891edfd56d14b5a3fe3e1063dddab54db2d
emptycastle <doyak92@naver.com>
2020-04-28 20:54:15 +0900

    lv3 동적계획법(Dynamic Programming)

---
+++ Algorithm/PROGRAMMERS/N으로 표현.py
@@ -0,0 +1,17 @@
+def solution(N, number):
+    answer, numli = -1, []
+    for i in range(1, 9):
+        numbers = set(); numbers.add( int(str(N) * i) )
+        for j in range(i - 1):
+            for x1 in numli[j]:
+                for x2 in numli[-1 - j]:
+                    numbers.add(x1 + x2)
+                    numbers.add(x1 - x2)
+                    numbers.add(x1 * x2)
+                    if x2 != 0:
+                        numbers.add(x1 // x2)
+        if number in numbers:
+            answer = i
+            break
+        numli.append(numbers)
+    return answer
