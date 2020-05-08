cebd269fba29ec7adc3ebb1122c898cf84194085
emptycastle <doyak92@naver.com>
2020-05-08 13:42:57 +0900

    lv3 탐욕법(Greedy)

--- Algorithm/PROGRAMMERS/단속카메라.py
+++ Algorithm/PROGRAMMERS/단속카메라.py
@@ -1,12 +1,9 @@
 def solution(routes):
-    answer = 0
     routes.sort(key=lambda x: x[1])
-    checked = [0] * len(routes)
-    while 0 in checked:
-        camera = routes[checked.index(0)][1]
-        checked[checked.index(0)] = 1
-        answer += 1
-        for i in range(1, len(routes)):
-            if routes[i][0] <= camera <= routes[i][1] and checked[i] != 1:
-                checked[i] = 1
+    camera = routes[0][1]
+    answer = 1
+    for i in range(1, len(routes)):
+        if camera < routes[i][0]:
+            answer += 1
+            camera = routes[i][1]
     return answer
