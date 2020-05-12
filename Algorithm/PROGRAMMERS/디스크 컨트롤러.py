3c4e3d9e78834b08564686c9c43ffd4bb6869d12
emptycastle <doyak92@naver.com>
2020-05-12 21:42:05 +0900

    lv3 힙(Heap)

---
+++ Algorithm/PROGRAMMERS/디스크 컨트롤러.py
@@ -0,0 +1,21 @@
+def solution(jobs):
+    n = len(jobs)
+    jobs.sort(key=lambda x: (x[0], x[1]))
+    start, time = jobs.pop(0)
+    answer, end = time, time + start
+    while jobs:
+        ni = 0
+        for i in range(1, len(jobs)):
+            if jobs[i][0] > end:
+                break
+            else:
+                if jobs[i][1] < jobs[ni][1]:
+                    ni = i
+        next = jobs.pop(ni)
+        if next[0] <= end:
+            answer += next[1] + (end - next[0])
+            end += next[1]
+        else:
+            answer += next[1]
+            end = sum(next)
+    return answer // n
