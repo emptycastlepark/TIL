d098fab483c356dfb9fde1c4214853e51c781cde
emptycastle <doyak92@naver.com>
2020-05-21 13:21:34 +0900

    D3/Programming Advanced/그래프의 기본과 탐색

---
+++ Algorithm/SWEA/5248. 그룹 나누기.py
@@ -0,0 +1,19 @@
+for tc in range(1, int(input()) + 1):
+    N, M = map(int, input().split())
+    paper = list(map(int, input().split()))
+    group = [[]*(N+1) for _ in range(N+1)]
+    for i in range(0, len(paper) - 1, 2):
+        group[paper[i]].append(paper[i+1]); group[paper[i+1]].append(paper[i])
+    cnt, checked = 0, [0] * (N + 1)
+    for i in range(len(paper)):
+        if checked[paper[i]] == 0:
+            queue = [paper[i]]; checked[paper[i]] = 1
+            while queue:
+                q = queue.pop(0)
+                for i in group[q]:
+                    if checked[i] == 0:
+                        checked[i] = 1
+                        queue.append(i)
+            cnt += 1
+    cnt += checked.count(0) - 1
+    print('#{} {}'.format(tc, cnt))
