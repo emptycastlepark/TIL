782060789f7f13e4263faa5db05f2de5815210da
emptycastle <doyak92@naver.com>
2020-05-22 12:32:40 +0900

    D4/Programming Advanced/그래프의 기본과 탐색

--- Algorithm/SWEA/5247. 연산.py
+++ Algorithm/SWEA/5247. 연산.py
@@ -8,17 +8,9 @@ for tc in range(1, int(input())+1):
         num, cnt = queue.popleft()
         if num == M:
             break
-        for i in range(4):
-            tempnum = 0
-            if i == 0:
-                tempnum = num + 1
-            elif i == 1:
-                tempnum = num - 1
-            elif i == 2:
-                tempnum = num * 2
-            else:
-                tempnum = num - 10
-            if 0 < tempnum <= 1000000 and checked[tempnum] == 0:
-                checked[tempnum] = cnt + 1
-                queue.append((tempnum, cnt + 1))
+        tempnum = [num + 1, num - 1, num * 2, num - 10]
+        for tn in tempnum:
+            if 0 < tn <= 1000000 and checked[tn] == 0:
+                checked[tn] = cnt + 1
+                queue.append((tn, cnt + 1))
     print('#{} {}'.format(tc, checked[M]))
