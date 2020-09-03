c3cc1c5ca10fd201f6d16b4ae1589d2273b1b734
emptycastle <doyak92@naver.com>
2020-09-03 22:16:50 +0900

    SW역량테스트 준비

--- Algorithm/BOJ/1063. 킹.py
+++ Algorithm/BOJ/1063. 킹.py
@@ -1,85 +1,21 @@
+dir = {'R': (0, 1), 'L': (0, -1), 'B': (1, 0), 'T': (-1, 0),
+       'RT': (-1, 1), 'LT': (-1, -1), 'RB': (1, 1), 'LB': (1, -1)}
+
 kd, sd, N = map(str, input().split())
 kd = [8 - int(kd[1]), ord(kd[0]) - 65]
 sd = [8 - int(sd[1]), ord(sd[0]) - 65]
 
 for _ in range(int(N)):
     com = input()
-    if com == 'R':
-        if kd[1] + 1 < 8:
-            if kd[0] != sd[0] or kd[1] + 1 != sd[1]:
-                kd[1] += 1
-            else:
-                if sd[1] + 1 < 8:
-                    kd[1] += 1
-                    sd[1] += 1
-    elif com == 'L':
-        if kd[1] - 1 >= 0 :
-            if kd[0] != sd[0] or kd[1] - 1 != sd[1]:
-                kd[1] -= 1
-            else:
-                if sd[1] - 1 >= 0:
-                    kd[1] -= 1
-                    sd[1] -= 1
-    elif com == 'B':
-        if kd[0] + 1 < 8:
-            if kd[0] + 1 != sd[0] or kd[1] != sd[1]:
-                kd[0] += 1
-            else:
-                if sd[0] + 1 < 8:
-                    kd[0] += 1
-                    sd[0] += 1
-    elif com == 'T':
-        if kd[0] - 1 >= 0:
-            if kd[0] - 1 != sd[0] or kd[1] != sd[1]:
-                kd[0] -= 1
-            else:
-                if sd[0] - 1 >= 0:
-                    kd[0] -= 1
-                    sd[0] -= 1
-    elif com  == 'RT':
-        if kd[0] - 1 >= 0 and kd[1] + 1 < 8:
-            if kd[0] - 1 != sd[0] or kd[1] + 1 != sd[1]:
-                kd[0] -= 1
-                kd[1] += 1
-            else:
-                if sd[0] - 1 >= 0 and sd[1] + 1 < 8:
-                    kd[0] -= 1
-                    kd[1] += 1
-                    sd[0] -= 1
-                    sd[1] += 1
-    elif com  == 'LT':
-        if kd[0] - 1 >= 0 and kd[1] - 1 >= 0:
-            if kd[0] - 1 != sd[0] or kd[1] - 1 != sd[1]:
-                kd[0] -= 1
-                kd[1] -= 1
-            else:
-                if sd[0] - 1 >= 0 and sd[1] - 1 >= 0:
-                    kd[0] -= 1
-                    kd[1] -= 1
-                    sd[0] -= 1
-                    sd[1] -= 1
-    elif com  == 'RB':
-        if kd[0] + 1 < 8 and kd[1] + 1 < 8:
-            if kd[0] + 1 != sd[0] or kd[1] + 1 != sd[1]:
-                kd[0] += 1
-                kd[1] += 1
-            else:
-                if sd[0] + 1 < 8 and sd[1] + 1 < 8:
-                    kd[0] += 1
-                    kd[1] += 1
-                    sd[0] += 1
-                    sd[1] += 1
-    else:
-        if kd[0] + 1 < 8 and kd[1] - 1 >= 0:
-            if kd[0] + 1 != sd[0] or kd[1] - 1 != sd[1]:
-                kd[0] += 1
-                kd[1] -= 1
-            else:
-                if sd[0] + 1 < 8 and sd[1] - 1 >= 0:
-                    kd[0] += 1
-                    kd[1] -= 1
-                    sd[0] += 1
-                    sd[1] -= 1
+    nkr, nkc = kd[0] + dir[com][0], kd[1] + dir[com][1]
+    nsr, nsc = sd[0] + dir[com][0], sd[1] + dir[com][1]
+    if 0 <= nkr < 8 and 0 <= nkc < 8:
+        if nkr != sd[0] or nkc != sd[1]:
+            kd[0], kd[1] = nkr, nkc
+        else:
+            if 0 <= nsr < 8 and 0 <= nsc < 8:
+                kd[0], kd[1] = nkr, nkc
+                sd[0], sd[1] = nsr, nsc
 
 print(str(chr(65 + kd[1])) + str(8 - kd[0]))
 print(str(chr(65 + sd[1])) + str(8 - sd[0]))
